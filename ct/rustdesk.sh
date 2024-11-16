ad45d0dc57d689ec0ec238e2d5649cdcdff3c708
kos4live <git@mail.go2inter.net>
2024-11-16 22:36:11 +0100

    Add RustDesk LXC

---
+++ ct/rustdesk.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: kos4live
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    ____             __  ____            __
+   / __ \__  _______/ /_/ __ \___  _____/ /__
+  / /_/ / / / / ___/ __/ / / / _ \/ ___/ //_/
+ / _, _/ /_/ (__  ) /_/ /_/ /  __(__  ) ,<
+/_/ |_|\__,_/____/\__/_____/\___/____/_/|_|
+ OSS / Docker
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="RustDesk OSS"
+var_disk="4"
+var_cpu="2"
+var_ram="1024"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/rustdesk-server ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+
+msg_info "Updating ${APP}"
+mv /opt/rustdesk-server/compose.yml /opt/rustdesk-server/compose.yml.bak-$(date +%Ymd)
+wget -q https://rustdesk.com/oss.yml -O /opt/rustdesk-server/compose.yml
+docker compose up -d
+msg_ok "Updated ${APP}"
+
+msg_ok "Updated Successfully"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
---
+++ install/rustdesk-install.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 community-scripts ORG, 2021-2024 tteck
+# Author: kos4live
+# Co-Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+get_latest_release() {
+  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+wait_for_file() {
+    local file="$1"; shift
+    local wait_seconds="${1:-15}"; shift
+
+    until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
+
+    test $wait_seconds -ge 0
+}
+
+DOCKER_LATEST_VERSION=$(get_latest_release "moby/moby")
+PORTAINER_LATEST_VERSION=$(get_latest_release "portainer/portainer")
+PORTAINER_AGENT_LATEST_VERSION=$(get_latest_release "portainer/agent")
+DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
+
+msg_info "Installing Docker $DOCKER_LATEST_VERSION"
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+mkdir -p $(dirname $DOCKER_CONFIG_PATH)
+echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
+$STD sh <(curl -sSL https://get.docker.com)
+msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
+
+read -r -p "Would you like to add Portainer? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
+  docker volume create portainer_data >/dev/null
+  $STD docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name=portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+  msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
+else
+  read -r -p "Would you like to add the Portainer Agent? <y/N> " prompt
+  if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+    msg_info "Installing Portainer agent $PORTAINER_AGENT_LATEST_VERSION"
+    $STD docker run -d \
+      -p 9001:9001 \
+      --name portainer_agent \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+      portainer/agent
+    msg_ok "Installed Portainer Agent $PORTAINER_AGENT_LATEST_VERSION"
+  fi
+fi
+
+msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+
+msg_info "Installing ${APP}"
+mkdir -p /opt/rustdesk-server
+wget -q https://rustdesk.com/oss.yml -O /opt/rustdesk-server/compose.yml
+docker compose up -d
+wait_for_file "/opt/rustdesk-server/data/id_ed25519.pub" && {
+    msg_info "RustDesk client Key: $(cat /opt/rustdesk-server/data/id_ed25519.pub)"
+}
+msg_ok "Installed ${APP}"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
